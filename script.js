138ee2d4e7b8c0785f0fbf6f41cb19edc5c4cef6
Shreyaan Seth <Shreyaans41@gmail.com>
2022-04-02 17:07:55 +0530

    search

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+setTimeout(() => {
+ let element = document.querySelectorAll("#endpoint");
+ let nonExistentFirstElement = element[2];
+  if (nonExistentFirstElement) nonExistentFirstElement.remove();
+}, 1000);
 
-let element = undefined
-let nonExistentFirstElement = undefined
-let count = 0
+setTimeout(() => {
 
-while (nonExistentFirstElement == undefined || count >=30) {
-  setTimeout(() => {
-    element = document.querySelectorAll('#endpoint');
-    nonExistentFirstElement = element[2];
-    if (nonExistentFirstElement) nonExistentFirstElement.remove();
-    console.log(count + " sgwks ")
+  let output = [];
+  let regex =/shorts*/gm
+  for (let i of document.querySelectorAll('*')) {
+      if (regex.test(i.href)) { // or whatever attribute you want to search
+          output.push(i);
+          var bruh = i.parentElement
+          bruh.parentElement.remove()
+      }
+  }
+  // output[0] && output.remove()
+ }, 1000);
+ 
 
-  }, 100);
-  count +=count
-}
