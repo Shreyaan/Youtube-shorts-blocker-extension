2f3bee63bbda1c2fa3d8dfd1d2e416048bb6181e
Shreyaan Seth <Shreyaans41@gmail.com>
2022-04-06 18:51:27 +0530

    shorts player too

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 setTimeout(() => {
- let element = document.querySelectorAll("#endpoint");
- let nonExistentFirstElement = element[2];
+  let element = document.querySelectorAll("#endpoint");
+  let nonExistentFirstElement = element[2];
   if (nonExistentFirstElement) nonExistentFirstElement.remove();
 }, 1000);
 
-setTimeout(() => {
-
+function removeShorts() {
   let output = [];
-  let regex =/shorts*/gm
-  for (let i of document.querySelectorAll('*')) {
-      if (regex.test(i.href)) { // or whatever attribute you want to search
-          output.push(i);
-          var bruh = i.parentElement
-          bruh.parentElement.remove()
-      }
+  let regex = /shorts*/gm;
+  for (let i of document.querySelectorAll("*")) {
+    if (regex.test(i.href)) {
+      // or whatever attribute you want to search
+      output.push(i);
+      var bruh = i.parentElement;
+      bruh.parentElement.remove();
+    }
   }
-  // output[0] && output.remove()
- }, 1000);
- 
 
+  let shortsPlayer= document.getElementById('shorts-player')
+  let shortsPlayerParent 
+if (shortsPlayer) shortsPlayerParent = shortsPlayer.parentElement
+  if(shortsPlayerParent != undefined || null) {shortsPlayerParent.parentElement.parentElement.remove()}
+
+}
+
+setTimeout(() => {
+  removeShorts();
+}, 1000);
+
+var time = 1;
+
+var interval = setInterval(function () {
+  if (time <= 100) {
+    removeShorts();
+
+    time++;
+  } else {
+    clearInterval(interval);
+  }
+}, 1000);
